8b8e048c79ffbf4c4f924193d3e051eac154f740
Parvez141300 <parvezhossain744471@gmail.com>
2025-06-17 12:58:10 +0600

    added popular courses and popular card

--- src/pages/Home/Shared/PopularCourses.jsx
+++ src/pages/Home/Shared/PopularCourses.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import CourseCard from './CourseCard';
 
 const PopularCourses = () => {
+    const [popularCourse, setPopularCourse] = useState([]);
+    console.log(popularCourse);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/enrolled-courses/popular')
+        .then(res => {
+            const data = res.data;
+            setPopularCourse(data)
+        })
+        .catch(error => {
+            toast.error(`${error.message}`)
+        })
+        
+    }, [])
+
+
+
     return (
-        <div>
-            <h2 className="text-3xl font-bold">Popular Courses</h2>
+        <div className='space-y-5'>
+            <h2 className="text-3xl font-bold">Popular Courses: ({popularCourse?.length})</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+                {
+                    popularCourse.map(course => <CourseCard key={course._id} course={course}></CourseCard>)
+                }
+            </div>
         </div>
     );
 };
